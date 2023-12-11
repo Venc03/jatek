65288bb4965932f9a53bb3ef4b9a4f17859671c8
Venc03 <vegi.dani1@gmail.com>
2023-12-11 13:59:47 +0100

    item pickup methods

--- src/jatek/Character.java
+++ src/jatek/Character.java
@@ -5,8 +5,28 @@ import java.util.Random;
 
 public class Character {
     Random rnd;
-    int hp = rnd.nextInt(6)*2+12;
-    int agility = rnd.nextInt(6)*+6;
-    int luck = rnd.nextInt(6)*+6;
-    ArrayList <String> equipment;
+    int hp, agility, luck;
+    ArrayList <Equipment> item;  
+
+    public Character(int hp, int agility, int luck) {
+        this.hp = roll()+12;
+        this.agility = roll()+6;
+        this.luck = roll()+6;
+    }
+    
+    private int roll(){
+        return rnd.nextInt(6)*2;
+    }
+    
+    public void pickup(Equipment item){
+        this.pickup(item, 1);
+    }
+    
+    public void pickup(Equipment item, int ammount){
+        for (int i = 0; i < ammount; i++) {
+            this.item.add(item);
+            
+        }
+    }
+    
 }
---
+++ src/jatek/Equipment.java
@@ -0,0 +1,22 @@
+package jatek;
+
+class Equipment {
+    String name;
+    String description;
+    int UUID;
+
+    public Equipment(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Equipment(String name, String description, int UUID) {
+        this.name = name;
+        this.description = description;
+        this.UUID = UUID;
+    }
+
+    
+    
+    
+}
